212bc945704a84af48ceb4a0679b76f928d143ca
JiangDonglai98 <1312527651@qq.com>
2020-06-29 22:19:39 +0800

    last change

--- code-explore/Analysis-UK-HPI-full-file.py
+++ code-explore/Analysis-UK-HPI-full-file.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #%% read UK-HPI-full-file
-UK_HPI = pd.read_csv("raw-data/Other - UK-HPI-full-file-2019-03.csv", index_col=0, parse_dates=True)
+UK_HPI = pd.read_csv("raw-data/Other - UK-HPI-full-file-2019-03.csv")
+UK_HPI.iloc[:,0] = pd.to_datetime(UK_HPI.iloc[:,0], format = "%d/%m/%Y")
 UK_HPI.head(20)
-#%% select first four colums
-UK_HPI = UK_HPI.iloc[:, 0:4]
+#%% select first five colums
+UK_HPI = UK_HPI.iloc[:, 0:5]
 UK_HPI.head(20)
 # %% find if every region's collection dates are over one year long
 over_one_year = UK_HPI.groupby('RegionName').count()
@@ -15,10 +16,16 @@ over_one_year
 over_one_year.to_csv('over_one_year.csv',header = True)
 
 # %%  find the latest date of each region to make sure the date is 01/03/2109
-UK_HPI.reset_index(inplace=True)
 latest_year = UK_HPI[['Date', 'RegionName']].groupby('RegionName').max()
 
 # %% save result to csv file
 latest_year.to_csv('latest_year.csv',header = True)
+
+# %% get 12 samples of each RegionName list in time
+UK_HPI = UK_HPI.loc[(UK_HPI['Date'] > '2018-03-01'),:]
+UK_HPI.head(20)
+# %% find the average price of each Region with 2018Code
+average_price = UK_HPI.groupby('CouncilArea2018Code').mean()
+average_price
 
 # %%
