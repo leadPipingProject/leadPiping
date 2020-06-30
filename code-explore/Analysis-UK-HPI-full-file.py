866fe38e980559bf9e60459e6495573074fba660
leadPipingProject <67356100+leadPipingProject@users.noreply.github.com>
2020-06-30 11:18:27 +0800

    remove data output

--- code-explore/Analysis-UK-HPI-full-file.py
+++ code-explore/Analysis-UK-HPI-full-file.py
@@ -13,13 +13,13 @@ UK_HPI.head(20)
 over_one_year = UK_HPI.groupby('RegionName').count()
 over_one_year
 # %% save result to csv file
-over_one_year.to_csv('over_one_year.csv',header = True)
+# over_one_year.to_csv('over_one_year.csv',header = True)
 
 # %%  find the latest date of each region to make sure the date is 01/03/2109
 latest_year = UK_HPI[['Date', 'RegionName']].groupby('RegionName').max()
 
 # %% save result to csv file
-latest_year.to_csv('latest_year.csv',header = True)
+# latest_year.to_csv('latest_year.csv',header = True)
 
 # %% get 12 samples of each RegionName list in time
 UK_HPI = UK_HPI.loc[(UK_HPI['Date'] > '2018-03-01'),:]
